4be0a04ff9b4f2d679425e6da1c6c043dbb44366
Alex Yashin <wingy.nidhoggr@gmail.com>
2019-12-15 23:42:35 +0300

    partners module init

---
+++ local/modules/extyl.partnersoffers/include.php
@@ -0,0 +1,32 @@
+<?php
+spl_autoload_register(
+    function ($className)
+    {
+        $baseName = 'Extyl\Spasibo\Partners';
+        $className = trim(substr($className, strlen($baseName)), '\\');
+        $classPath = __DIR__ . '/lib/' . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+        if (file_exists($classPath)) {
+            require_once($classPath);
+        }
+    }
+);
+
+if ( ! function_exists('partnersoffers')) {
+    function partnersoffers()
+    {
+        return \Extyl\Spasibo\Partners\App::getInstance();
+    }
+}
+
+if ( ! function_exists('bxApp')) {
+    function bxApp()
+    {
+        global $APPLICATION;
+
+        return $APPLICATION;
+    }
+}
+
+if (ADMIN_SECTION !== true) {
+    partnersoffers()::initFacades();
+}
---
+++ local/templates/partnersoffers/footer.php
@@ -0,0 +1,8 @@
+</div></div>
+</main>
+
+<script src="/assets/js/jquery-3.4.1.min.js"></script>
+<script src="/assets/js/owl.carousel.min.js"></script>
+<script src="/assets/js/custom.js"></script>
+</body>
+</html>
